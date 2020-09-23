55519c62a20e12694e5e96fc2de78b43d04ca182
Roberto <robertoarayaday@gmail.com>
2020-09-23 13:30:16 -0300

    Upload 23/09

--- Desarrollo de aplicaciones web/Tareas/Tarea1/the_javascript.js
+++ Desarrollo de aplicaciones web/Tareas/Tarea1/the_javascript.js
@@ -1,21 +1,39 @@
 function informarMascotas(){
     const x = document.getElementById("informar");
     const y = document.getElementById("listado");
+    const z = document.getElementById("estadistica");
     if (x.style.display==='block'){
         x.style.display="none";
     } else {
         x.style.display='block';
-        y.style.display='none'
+        y.style.display='none';
+        z.style.display='none';
     }
 }
 
 function mostrarListado(){
     let x = document.getElementById("listado");
     const y = document.getElementById("informar");
+    const z = document.getElementById("estadistica");
     if (x.style.display==='block'){
         x.style.display="none";
     } else {
         x.style.display='block';
-        y.style.display='none'
+        y.style.display='none';
+        z.style.display='none';
+    }
+}
+
+
+function mostrarEstadistica(){
+    let x = document.getElementById("estadistica");
+    const y = document.getElementById("listado");
+    const z = document.getElementById("informar");
+    if (x.style.display==='block'){
+        x.style.display="none";
+    } else {
+        x.style.display='block';
+        y.style.display='none';
+        z.style.display='none'
     }
 }
